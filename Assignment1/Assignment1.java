7924d23e8c38c17d48a7e7b0afb2ed1442c44118
Ishita Jain <er.ishitajain@gmail.com>
2021-07-26 15:40:59 +0530

    Did corrections in DownloadWebPage function. Instead of downloading it in a html file, it only takes the text content of web page and write it in a text file with all the html tags removed, in the form of a string.

--- Assignment1/Assignment1.java
+++ Assignment1/Assignment1.java
@@ -10,29 +10,35 @@ import java.io.*;
 public class Assignment1{
     public static void main(String[] args)
     {
-        
+        String url="https://docs.oracle.com/javase/6/docs/api/java/util/regex/package-summary.html";
+        Download.DownloadWebPage(url);
     }
     
 }
+
+
 /**
  * Read and download web page content from given URLs to a html file.
  * @param url an URL object corresponding to the address of the web page 
- * @param reader a buffered reader to read the input stream from URL
- * @param writer connect the buffered writer stream to file object where web content will be downloaded
- * @param line string type variable to read lines from web page
- * @return content at particular web page corresponding to the given URL
+ * @param sc a scanner class object to read content of web page using openStream()
+ * @param sb an empty string buffer object to append the content of web page in html format
+ * @param result a string object in which string buffer sb will be converted and html tags are removed using replaceAll(regex, "")
+ * @param writer a buffered writer object to write the result in a text file
+ * @return text content at particular web page corresponding to the given URL. Save it in Content.txt in a form of a string
  */
 class Download{
     public static void DownloadWebPage(String link){
         try{
             URL url=new URL(link);
-            BufferedReader reader=new BufferedReader(new InputStreamReader(url.openStream()));
-            BufferedWriter writer= new BufferedWriter(new FileWriter("Download.html"));
-            String line;
-            while((line=reader.readLine())!=null){
-                writer.write(line);
+            Scanner sc=new Scanner(url.openStream());
+            StringBuffer sb=new StringBuffer();
+            while(sc.hasNext()){
+                sb.append(sc.next());
             }
-            reader.close();
+            String result=sb.toString();
+            result=result.replaceAll("\\<.*?\\>","");
+            BufferedWriter writer= new BufferedWriter(new FileWriter("Content.txt"));
+            writer.write(result);
             writer.close();
         }
         catch ( MalformedURLException merror){
@@ -43,3 +49,4 @@ class Download{
         }
     }
 }
+
