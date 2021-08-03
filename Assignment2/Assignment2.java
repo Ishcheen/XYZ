7325902bde14cfa7bf4fcf7cefe4aa0ae93ff301
Ishita Jain <er.ishitajain@gmail.com>
2021-08-04 01:05:02 +0530

    Added shape menu to display on console. Provide the user with available shapes option and ask an integer input corresponding to index of his choice.

---
+++ Assignment2/Assignment2.java
@@ -0,0 +1,17 @@
+/*
+ * Classname: Assigment2.java
+ */
+import java.util.*;
+
+public class Assignment2{
+    public static void main(String[] args){
+        System.out.println("Select the shape to calculate the area and perimeter for: 
+                            \n 1. Circle \n 2. Rectangle \n 3. Triangle \n 4. Square \n 
+                            5. Sphere \n 6. Cylinder\n");
+        System.out.println("Enter the index number corresponding to your choice:");
+        Scanner sc=new Scanner(System.in);
+        String i=sc.nextInt();        
+    }
+}
+
+
