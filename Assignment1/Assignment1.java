e101d3a95828248659797bfceb075fbfb096be30
Ishita Jain <er.ishitajain@gmail.com>
2021-08-04 03:22:26 +0530

    Added relevant comments for each method and renamed the methods according to naming conventions.

--- Assignment1/Assignment1.java
+++ Assignment1/Assignment1.java
@@ -4,22 +4,25 @@
 import java.util.*;
 import java.net.*;
 import java.io.*;
-/**
- * 
- */
+
+
 public class Assignment1{
     public static void main(String[] args)
     {
-        ReadAFile.ReadUrls();
-        ReadAFile.ReadWords();
+        ReadAFile.readUrls();
+        ReadAFile.readWords();
     }
     
 }
 
 
 class ReadAFile{
-    
-    public static void ReadUrls(){
+    /**
+     * readUrls method-
+     * For each url present in urls.txt, it calculates the count for first three words 
+     * present in words.txt and display it in Output1.txt.
+     */
+    public static void readUrls(){
         try{
             File urlsFile= new File("./urls.txt");
             FileReader fileReader=new FileReader(urlsFile);
@@ -27,7 +30,7 @@ class ReadAFile{
             String line;
             BufferedWriter writer= new BufferedWriter(new FileWriter("Output1.txt"));
             while((line=reader.readLine())!=null){
-                DownloadWebPage(line);
+                downloadWebPage(line);
                 writer.write(line);
                 writer.write("\n");
                
@@ -38,7 +41,7 @@ class ReadAFile{
                 int n=0;
                 while((word=rdr.readLine())!=null && n<3){
                     n++;
-                    int count=MatchCount(word);
+                    int count=matchCount(word);
                     writer.write(word+"-"+count);
                     writer.write("\n");
                 }
@@ -49,8 +52,12 @@ class ReadAFile{
             System.out.println("IO Exception raised");
         }
     }
-    
-    public static void ReadWords(){
+    /**
+     * readWords method-
+     * For each word present in words.txt, it calculates its total occurence in websites corresponding to all links of urls.txt file.
+     * The total count is written in Output2.txt file in the format 'word-count'
+     */
+    public static void readWords(){
         try{
             File wordFile=new File("./words.txt");
             FileReader fileRdr=new FileReader(wordFile);
@@ -64,8 +71,8 @@ class ReadAFile{
                 String line;
                 BufferedWriter writer= new BufferedWriter(new FileWriter("Output2.txt",true));
                 while((line=reader.readLine())!=null){
-                    DownloadWebPage(line);
-                    count+=MatchCount(word);
+                    downloadWebPage(line);
+                    count+=matchCount(word);
                 }
                 writer.write(word+"-"+count);
                 writer.write("\n");
@@ -77,7 +84,13 @@ class ReadAFile{
         }
         
     }
-    public static int MatchCount(String word){
+    /**
+     * matchCount method-
+     * It takes a word as argument and count its occurence in Content.txt file
+     * @param index -Index of occurence of word in Content.txt file
+     * @param count- Total count of occurence of given word in Content.txt file
+     */
+    public static int matchCount(String word){
         if(word==null){
             return 0;
         }
@@ -106,8 +119,16 @@ class ReadAFile{
          
         return count;
     }
-
-    public static void DownloadWebPage(String link){
+    
+    /**
+     * downloadWebPage method-
+     * It takes an url as an argument and store the html document corresponding to the url in String Buffer sb.
+     * Text content visible on the website is extracted from html doc and is written in Content.txt file.
+     * @param url -URL type object of the required website link.
+     * @param sb- String Buffer to store the website content
+     * @param result- Data is stored in the String format in result from sb to be written in the file Content.txt  
+     */
+    public static void downloadWebPage(String link){
         try{
             URL url=new URL(link);
             Scanner sc=new Scanner(url.openStream());
