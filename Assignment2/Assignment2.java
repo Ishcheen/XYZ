6ebb85c5169371d2bc7436d3e987285cb035e43b
Ishita Jain <er.ishitajain@gmail.com>
2021-08-04 02:17:46 +0530

    Continuous execution of program without exiting Made changes in main to run continuously until the user enters -1.

--- Assignment2/Assignment2.java
+++ Assignment2/Assignment2.java
@@ -16,32 +16,38 @@ public class Assignment2{
     public static void main(String[] args){
         System.out.println("Select the shape to calculate the area and perimeter for:"+ 
                             "\n 1. Circle \n 2. Rectangle \n 3. Triangle \n 4. Square \n"+ 
-                            "5. Sphere \n 6. Cylinder\n");
-        System.out.println("Enter the index number corresponding to your choice:");
-        Scanner sc=new Scanner(System.in);
-        int i=sc.nextInt(); 
-        if(i==1){
-            Circle obj=new Circle();
+                            " 5. Sphere \n 6. Cylinder\n ");
+        while(true){
+            System.out.println("Enter the index number corresponding to your choice or enter '-1' to exit :");
+            Scanner sc=new Scanner(System.in);
+            int i=sc.nextInt(); 
+            if(i==-1){
+                break;
+            }
+            else if(i==1){
+                Circle obj=new Circle();
+            }
+            else if(i==2){
+                Rectangle obj=new Rectangle();
+            }
+            else if(i==3){
+                Triangle obj=new Triangle();
+            }
+            else if(i==4){
+                Square obj=new Square();
+            }
+            else if(i==5){
+                Sphere obj=new Sphere();
+            }
+            else if(i==6){
+                Cylinder obj=new Cylinder();
+            }
+            else
+            {
+                System.out.println("Enter a valid index");
+            }
         }
-        else if(i==2){
-            Rectangle obj=new Rectangle();
-        }
-        else if(i==3){
-            Triangle obj=new Triangle();
-        }
-        else if(i==4){
-            Square obj=new Square();
-        }
-        else if(i==5){
-            Sphere obj=new Sphere();
-        }
-        else if(i==6){
-            Cylinder obj=new Cylinder();
-        }
-        else
-        {
-            System.out.println("Enter a valid index");
-        }       
+               
     }
 }
 
@@ -194,7 +200,7 @@ class Cylinder extends Shape{
     }
 }
 
-class Triangle{
+class Triangle extends Shape{
     private double height;
     private double base;
     private double a;
