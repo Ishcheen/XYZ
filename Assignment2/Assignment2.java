c8b0765c53721f669ea2b617566af502a4162908
Ishita Jain <er.ishitajain@gmail.com>
2021-08-04 01:48:18 +0530

    Added class Circle extending Shape class Circle class has a private field radius with its getter setter method. setRadius method uses setInput method of Shape class to check the input validity. Once the radius is set, its area and circumference is calculated by using area and perimeter method of parent class Shape.

--- Assignment2/Assignment2.java
+++ Assignment2/Assignment2.java
@@ -14,30 +14,30 @@ interface ShapeInterface{
 
 public class Assignment2{
     public static void main(String[] args){
-        System.out.println("Select the shape to calculate the area and perimeter for: 
-                            \n 1. Circle \n 2. Rectangle \n 3. Triangle \n 4. Square \n 
-                            5. Sphere \n 6. Cylinder\n");
+        System.out.println("Select the shape to calculate the area and perimeter for:"+ 
+                            "\n 1. Circle \n 2. Rectangle \n 3. Triangle \n 4. Square \n"+ 
+                            "5. Sphere \n 6. Cylinder\n");
         System.out.println("Enter the index number corresponding to your choice:");
         Scanner sc=new Scanner(System.in);
-        String i=sc.nextInt(); 
+        int i=sc.nextInt(); 
         if(i==1){
             Circle obj=new Circle();
         }
-        else if(i==2){
-            Rectangle obj=new Rectangle();
-        }
-        else if(i==3){
-            Triangle obj=new Triangle();
-        }
-        else if(i==4){
-            Square obj=new Square();
-        }
-        else if(i==5){
-            Sphere obj=new Sphere();
-        }
-        else if(i==6){
-            Cylinder obj=new Cylinder();
-        }
+        // else if(i==2){
+        //     Rectangle obj=new Rectangle();
+        // }
+        // else if(i==3){
+        //     Triangle obj=new Triangle();
+        // }
+        // else if(i==4){
+        //     Square obj=new Square();
+        // }
+        // else if(i==5){
+        //     Sphere obj=new Sphere();
+        // }
+        // else if(i==6){
+        //     Cylinder obj=new Cylinder();
+        // }
         else
         {
             System.out.println("Enter a valid index");
@@ -92,5 +92,22 @@ class Shape implements ShapeInterface{
             }
         }
         return 0;
+    }
+}
+
+
+class Circle extends Shape{
+    private double radius;
+    public void setRadius(){
+        this.radius=this.setInput();
+    }
+    public double getRadius(){
+        return this.radius;
+    }
+    Circle(){
+        System.out.println("Enter the radius of the circle:");
+        this.setRadius();
+        System.out.println("Area of the circle is "+this.PI*this.area(this.radius));
+        System.out.println("Circumference of the circle is "+2*this.PI*this.perimeter(this.radius));
     }
 }
