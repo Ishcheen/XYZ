bf36943d288c3c0e448a78263a1774fa4b32ad3a
Ishita Jain <er.ishitajain@gmail.com>
2021-08-04 19:12:28 +0530

    Created class Account With name and balance field, initial value of balance=1000 and getBalance and withdraw method.

--- Assignment4/Assignment4.java
+++ Assignment4/Assignment4.java
@@ -3,3 +3,13 @@
  */
 import java.util.*;
 
+class Account{
+    private String name;
+    private int balance=1000;
+    public int getBalance(){
+        return this.balance;
+    }
+    public void withdraw(int amount){
+        balance= balance-amount;
+    }
+}
