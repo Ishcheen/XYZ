5940ee88b483686388a2cb0f1bddc8d98f2402ac
Ishita Jain <er.ishitajain@gmail.com>
2021-08-05 15:01:54 +0530

    Added class SerializationTest Added four Student objects and serialized the result to two file output1.ser and output2.ser using command line argument. Made four Student objects null.

--- Assignment5/Assignment5.java
+++ Assignment5/Assignment5.java
@@ -6,11 +6,11 @@ import java.io.*;
 
 public class Assignment5{
     public static void main(String[] args){
-        Student s1=new Student("Ishita", "31-10-1999");
-        s1.address=new Address("Indore", "MP", 452030, "India");
+        SerializationTest st=new SerializationTest();
+        st.main(args[0]);
     }
 }
-class Student{
+class Student implements Serializable{
     private String firstName;
     private String dateOfBirth;
     Address address;
@@ -26,7 +26,7 @@ class Student{
     }
 }
 
-class Address{
+class Address implements Serializable{
     private String city;
     private String state;
     private int pinCode;
@@ -48,5 +48,33 @@ class Address{
     }
     public String getCountry(){
         return this.country;
+    }
+}
+
+class SerializationTest{
+    public static void main(String arg1){
+        Student s1=new Student("Ishita", "31-10-1999");
+        s1.address=new Address("Indore", "MP", 452030, "India");
+        Student s2=new Student("Jack", "31-07-2000");
+        s2.address=new Address("Bhopal", "MP", 472030, "India");
+        Student s3=new Student("Oliver", "20-01-1990");
+        s3.address=new Address("Jaipur", "Rajasthan", 422030, "India");
+        Student s4=new Student("Shinchan", "05-03-2010");
+        s4.address=new Address("Mumbai", "Maharastra", 462001, "India");
+        try{
+            FileOutputStream fs=new FileOutputStream(arg1);
+            ObjectOutputStream os= new ObjectOutputStream(fs);
+            os.writeObject(s1);
+            os.writeObject(s2);
+            os.writeObject(s3);
+            os.writeObject(s4);
+            os.close();
+        }catch (IOException ex){
+            System.out.println("IO Exception raised");
+        }
+        s1=null;
+        s2=null;
+        s3=null;
+        s4=null;
     }
 }
