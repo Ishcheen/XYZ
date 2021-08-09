d814e803e37763210fefd7f4b4f4046d7a028d7a
Ishita Jain <er.ishitajain@gmail.com>
2021-08-10 02:38:33 +0530

    Created the class Employee with required fields and getter-setter methods.

--- assignment3/Assignment3.java
+++ assignment3/Assignment3.java
@@ -1,8 +1,38 @@
 /*
  * Classname: Assigment3.java
  */
+package assignment3;
+
 import java.util.*;
 import java.io.*;
-/**
- * 
- */
+
+class Employee{
+    private String name;
+    private String emailId;
+    private int age;
+    private String dateOfBirth;
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setEmailId(String EmailId){
+        this.emailId=EmailId;
+    }
+    public String getEmailId(){
+        return this.emailId;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public void setDOB(String dateOfBirth){
+        this.dateOfBirth=dateOfBirth;
+    }
+    public String getDOB(){
+        return this.dateOfBirth;
+    } 
+}
