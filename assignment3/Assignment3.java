16ca672344727d7f40cca6991ecbda54f11c4736
Ishita Jain <er.ishitajain@gmail.com>
2021-08-10 02:41:43 +0530

    Added createList method It read items from a file of Employee data and append it to an arraylist

--- assignment3/Assignment3.java
+++ assignment3/Assignment3.java
@@ -35,4 +35,24 @@ class Employee{
     public String getDOB(){
         return this.dateOfBirth;
     } 
+    public void createList(ArrayList<Employee> list){
+        try{
+            File employee=new File("employees.txt");
+            FileReader fileReader=new FileReader(employee);
+            BufferedReader br=new BufferedReader(fileReader);
+            String line;
+            while((line=br.readLine())!=null){
+                Employee obj=new Employee();
+                String[] str=line.split(",");
+                obj.setName(str[0]);
+                obj.setEmailId(str[1]);
+                obj.setAge(Integer.parseInt(str[2]));
+                obj.setDOB(str[3]);
+                list.add(obj);
+            }
+        }
+        catch ( IOException ex){
+            System.out.println("IO Exception raised");
+        }
+    }
 }
